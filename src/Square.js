2d778d6e6b5af9e983fdf923a9abcc2e6f3a352f
moocow <efriedla20852@gmail.com>
2021-06-29 16:12:05 -0400

    clickable square

--- src/Square.js
+++ src/Square.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default class Square extends React.Component{
     render() {
         return (
-          <button className="square">
-            {/* todo */ }
+          <button className="square" onClick={function() { alert('click'); }}>
+            {this.props.value}
           </button>
         );
       }
