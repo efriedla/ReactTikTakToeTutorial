4b8f0ba83efa9d19ceb7f056cde00f5d8feabade
moocow <efriedla20852@gmail.com>
2021-06-29 19:05:27 -0400

    Moved logic out of Board and into game with timer

--- src/Board.js
+++ src/Board.js
@@ -1,56 +1,17 @@
 import React from 'react';
-import calculateWinner from './calculateWinner';
 import Square from './Square';
 
 export default class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          squares: Array(9).fill(null),
-          xIsNext: true,
-        };
-      }
-
-      /** on click will alternate whos turn it is 
-       * Squares is an array that fits 9
-       * i represents the number in the array that is being clicked on
-       * xIsNExt is a boolean that will determine if x or o will be in the square
-       * this create a key value pair in the game
-      */
-      handleClick(i) {
-        const squares = this.state.squares.slice();
-        if(calculateWinner(squares) || squares[i]){
-            return; 
-        }
-        squares[i] = this.state.xIsNext ? 'X': 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-      }
-
       /** this will handle what will be in the squares */
     renderSquare(i) {
-        return <Square 
-        value={this.state.squares[i]} 
-        onClick={() => this.handleClick(i)}
-        />
+        return <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
     }
     render() {
-        // game logic
-        const winner = calculateWinner(this.state.squares);
-        let status = '';
-        if(winner) {
-            status = "Winner: " + winner;
-        }else{
-            status ='Next player: ' + (this.state.xIsNext ? 'X': 'O');
-        }
-
-        
-
         return (
           <div>
-            <div className="status">{status}</div>
             <div className="board-row">
               {this.renderSquare(0)}
               {this.renderSquare(1)}
