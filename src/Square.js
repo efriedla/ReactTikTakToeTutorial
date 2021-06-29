e4dff49819883321cb6f07e547de88b30fe0a99a
moocow <efriedla20852@gmail.com>
2021-06-29 17:50:15 -0400

    Square now post X in any Square

--- src/Square.js
+++ src/Square.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 export default class Square extends React.Component{
+    
     render() {
         return (
-          <button className="square" onClick={function() { alert('click'); }}>
+          <button className="square"
+          onClick={() => this.props.onClick()}
+        >
             {this.props.value}
           </button>
         );
